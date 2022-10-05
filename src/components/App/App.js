4786d845df1788efdeb7c29a65e29a689d910a4e
abelmuro <abelmuro93@gmail.com>
2022-10-05 11:42:01 -0700

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavigationBar from './NavigationBar';
+import './styles.css';
+
+
+function App() {
+    return(
+    <>
+        <NavigationBar />
+    </>
+    )
+}
+
+export default App;
---
+++ src/components/App/NavigationBar/NavigationBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './styles.module.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faHouse, faUser, faSheetPlastic} from '@fortawesome/free-solid-svg-icons';
+
+function NavigationBar() {
+    return(
+        <nav className={styles.navBar}>
+            <FontAwesomeIcon icon={faHouse} className={styles.icon} title={"Home"}/> 
+            <FontAwesomeIcon icon={faSheetPlastic} className={styles.icon} title={"Projects"}/>
+            <FontAwesomeIcon icon={faUser} className={styles.icon} title={"Contant Me"}/>
+        </nav>
+    )
+}
+
+export default NavigationBar;
