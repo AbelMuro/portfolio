8f84223afbea38b0728c4e3426ecbf606a1f37bd
abelmuro <abelmuro93@gmail.com>
2022-11-28 21:31:46 -0800

    commit

--- src/components/App/ContactMeSection/ContactMeSection.js
+++ src/components/App/ContactMeSection/ContactMeSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './styles.module.css';
 import TextField from '@mui/material/TextField';
 import Map from './Map';
@@ -6,13 +6,46 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 
 function ContactMeSection() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
 
-    const handleSubmit = (e) => {
-        let allForms = document.forms["reset"];
-        alert("Your Message has been submitted");
-        allForms.reset();        
-        e.preventDefault;
+
+    const handleName = (e) => {
+        setName(e.target.value)
     }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handleMessage = (e) => {
+        setMessage(e.target.value);
+    }
+    
+    const encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&");
+    }
+
+    const handleSubmit = async (e) => {
+        try{
+            alert("Message has been submitted")             //i am forced to put the alert before making the fetch request 
+            await fetch("/" , {                             //because netlify is not displaying the alert if i put it after the fetch request (not sure whats going on here)
+                method: "POST",
+                headers: {"Content-Type" : "application/x-www-form-urlencoded"},
+                body: encode({"form-name" : "contact", name: name, email: email, message: message})
+            });      
+            e.preventDefault();
+        }
+        catch(error){
+            alert(error);
+        }
+   
+    }
+
+    
 
     return(
         <section className={styles.container} id="contactMe">
@@ -25,23 +58,27 @@ function ContactMeSection() {
                         Questions? Concerns? Please message me 
                         and I will respond as soon as I can.
                     </p>
-                    <form className={styles.formContainer} id="reset" onSubmit={handleSubmit} name="contact" method="post">
-                        <TextField id="outlined-basic" label="Enter Name" variant="outlined" className={styles.inputs} type="text" name="name" required/>
+                    {/* note to my future self: please check the index.html file in the outer most folder in src*/}
+                    <form className={styles.formContainer} id="reset" onSubmit={handleSubmit}>                   
+                        <TextField id="outlined-basic" label="Enter Name" variant="outlined" className={styles.inputs} inputProps={{type: "text", name: "name"}} required value={name} onChange={handleName}/>
                         <br/>
-                        <TextField id="outlined-basic" label="Enter Email" variant="outlined" className={styles.inputs} type="email" name="email" required/>
+                        <TextField id="outlined-basic" label="Enter Email" variant="outlined" className={styles.inputs} inputProps={{type: "email", name: "email"}} required value={email} onChange={handleEmail}/>
                         <br/>
                         <TextField
-                            name="message"
+                            inputProps={{name: "message"}}
                             className={styles.inputMessage}
                             id="outlined-multiline-static"
                             label="Enter Message"
                             multiline
                             rows={8}
-                            required/>  
+                            required
+                            value={message}
+                            onChange={handleMessage}
+                            />  
                         <Box className={styles.button}>
                             <Button type="submit" variant="contained">Submit</Button>                             
                         </Box>
-                        <input type="hidden" name="form-name" value="contact" />
+
                     </form>                    
                 </div>
             </div>   
