5fbdf8c3d88f7dd7768bcb082f664d98690189ff
abelmuro <abelmuro93@gmail.com>
2022-11-29 21:09:59 -0800

    commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,3 +20,37 @@ function App() {
 }
 
 export default App;
+
+
+{/*  	
+function addNumbers() {
+    	let first = "2234535";
+        let second = "2765";
+    	let shortestString;
+    	if(first.length < second.length){
+        	shortestString = first
+        }
+        else
+        	shortestString = second;
+            
+        let carry = 0;
+        let totalSum = "";
+             
+        for(let x = 0; x < shortestString.length; x++){
+        	let a = parseInt(first.charAt(first.length - 1 - x));
+            let b = parseInt(second.charAt(second.length - 1 - x));
+            let currentSum = a + b + carry;
+            if(currentSum >= 10){
+            	currentSum = currentSum + "";
+                carry = 1;
+                totalSum += (currentSum.charAt(1) + "")
+            }
+            else{
+            	carry = 0;
+                totalSum += (currentSum + "");  
+            }                    	
+        }   
+        
+        console.log(totalSum);
+            
+    }*/}
