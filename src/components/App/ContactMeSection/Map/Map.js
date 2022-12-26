157a8068c3cf69ea87e4a971ebc6692e697286ac
abelmuro <abelmuro93@gmail.com>
2022-12-25 23:16:45 -0800

    commit

--- src/components/App/ContactMeSection/Map/Map.js
+++ src/components/App/ContactMeSection/Map/Map.js
@@ -1,41 +1,31 @@
 import React from 'react';
 import styles from './styles.module.css';
 import iAmHereIcon from './images/I am here icon.png';
-import {GoogleMap, useLoadScript} from '@react-google-maps/api';
+import {GoogleMap, useLoadScript, Marker} from '@react-google-maps/api';
 
 function Map() {
+
     const {isLoaded} = useLoadScript({
         googleMapsApiKey: process.env.GOOGLE_MAP_KEY,
     });
 
-    const geocoding = (map) => {
-        let geocoder = new google.maps.Geocoder();
-
-        geocoder.geocode({address: "2409 aberdeen way, Richmond, CA 94806"}, (results, status)=> { 
-            if(status != "OK"){ console.log("error"); return;}
-
-            map.setCenter(results[0].geometry.location); 
-            var marker = new google.maps.Marker({                 
-                position: results[0].geometry.location,  
-                map: map,
-                icon: iAmHereIcon                                 
-            })
-            map.setZoom(10); 
-        })
-    }
 
     return( isLoaded ? 
         <GoogleMap
             mapContainerClassName={styles.mapContainer}
-            onLoad={(map) => {geocoding(map)}}
+            center= {{lat: 37.9595694, lng: -122.3482515}}
             zoom={10}
             options={{
                 zoomControl: false,
                 streetViewControl: false,
                 mapTypeControl: false,
                 fullscreenControl: false,
-            }}
-        >
+            }}>
+
+            <Marker icon={{
+                url: iAmHereIcon,
+                scaledSize: new google.maps.Size(100, 100),
+            }} position={{lat: 37.9595694, lng: -122.3482515}}/>
 
         </GoogleMap> : <div> is loading...</div>
     )
