08cd1368ad0729cb07158d2dd97fddb4a2c341a4
Abel Muro <abelmuro93@gmail.com>
2023-07-08 22:19:48 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
         path: path.join(__dirname, '/dist'), 
         filename: 'bundle.js'                 
     },
+    systemvars: true,
     plugins: [                      
         new HtmlWebpackPlugin({              
             filename: 'index.html',   
