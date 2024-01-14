210d7f93c28a2aaf7acd001e2a53d5957fb9e3d7
abelmuro <abelmuro93@gmail.com>
2024-01-14 14:06:40 -0800

    commit

--- src/components/App/ProjectSection/ProjectSection.js
+++ src/components/App/ProjectSection/ProjectSection.js
@@ -35,7 +35,7 @@ function ProjectSection () {
             <Project href="https://tic-tac-toe-abel.netlify.app" src={images["ticTacToeGame"]} projectTitle="Tic Tac Toe Game"/>
             <Project href="https://entertainment-app-front-end.netlify.app" src={images["entertainmentApp"]} projectTitle="Entertainment App"/>
             <Project href="https://github-user-app-one.vercel.app/" src={images["githubUserApp"]} projectTitle="Github User Search App"/>
-            <Project href="https://password-generator-app-23e185feaac3.herokuapp.com/" src={images["passwordGeneratorApp"]} projectTitle="Password Generator App"/>
+            <Project href="https://password-generator-front-end-mentor.netlify.app/" src={images["passwordGeneratorApp"]} projectTitle="Password Generator App"/>
             <Project href="https://dev-jobs-app-front.netlify.app/" src={images["devJobsApp"]} projectTitle="Dev Jobs App"/>
             <Project href="https://link-sharing-boqxtcz3a-abelmuro.vercel.app/" src={images["linkSharingApp"]} projectTitle="Link Sharing App"/>
             <Project href="https://age-calculator-app-ashy.vercel.app/" src={images["ageCalculatorApp"]} projectTitle="Age Calculator App"/>
