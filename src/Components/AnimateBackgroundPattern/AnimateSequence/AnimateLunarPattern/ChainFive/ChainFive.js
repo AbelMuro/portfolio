16ada466048caed522459a4f628c5d882e635b77
abelmuro <abelmuro93@gmail.com>
2025-07-08 10:55:47 -0700

    e

--- src/Components/AnimateBackgroundPattern/AnimateSequence/AnimateLunarPattern/ChainFive/ChainFive.js
+++ src/Components/AnimateBackgroundPattern/AnimateSequence/AnimateLunarPattern/ChainFive/ChainFive.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import * as styles from './styles.module.css';
 
+/* this is where i left off, i need to finish the last bottom chain for the lunar design*/
+
 function ChainFive(){
     return(<></>)
 }
