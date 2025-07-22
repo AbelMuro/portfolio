148bcca3fc2d5ae54b5a04a219a2ab5c8a6665e3
abelmuro <abelmuro93@gmail.com>
2025-07-22 11:13:25 -0700

    e

--- src/Components/ContactMeSection/Form/Form.js
+++ src/Components/ContactMeSection/Form/Form.js
@@ -1,9 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import EnterName from './EnterName';
 import EnterEmail from './EnterEmail';
 import EnterMessage from './EnterMessage';
 import {motion, LayoutGroup} from 'framer-motion';
 import * as styles from './styles.module.css';
+
+
+/* 
+    this is where i left off, i need to finish the responsiveness of the following component
+    then i can either start developing the API for this app or i can continue fixing the visual bugs
+    with the scrolling animation
+*/
 
 function Form() {
 
