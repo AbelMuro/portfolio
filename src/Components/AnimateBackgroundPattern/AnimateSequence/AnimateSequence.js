9e062b12285ec3c424e4053cedea15bbbfe26a2e
abelmuro <abelmuro93@gmail.com>
2025-07-31 11:26:06 -0700

    c

--- src/Components/AnimateBackgroundPattern/AnimateSequence/AnimateSequence.js
+++ src/Components/AnimateBackgroundPattern/AnimateSequence/AnimateSequence.js
@@ -9,6 +9,9 @@ import AnimateSquarePattern from './AnimateSquarePattern';
 import AnimateLunarPattern from './AnimateLunarPattern';
 import * as styles from './styles.module.css';
 
+/* 
+    this is where i left off, i need to continue optimizing the components for safari and the other browsers
+*/
 
 function AnimateSequence() {
 
