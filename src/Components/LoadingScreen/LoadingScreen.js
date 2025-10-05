b8b4307c852caaed5c69f316516d1e57bdf98cc2
abelmuro <abelmuro93@gmail.com>
2025-10-05 09:43:54 -0700

    commit

--- src/Components/LoadingScreen/LoadingScreen.js
+++ src/Components/LoadingScreen/LoadingScreen.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {motion, AnimatePresence} from 'framer-motion';
 import ProgressBar from './ProgressBar';
 import * as styles from './styles.module.css';
 
 function LoadingScreen() {
-    return(
-        <section className={styles.loading}>
-            <div className={styles.content}>
-                <h2>Hold on.. let me think.</h2>
-                <ProgressBar/>              
-            </div>
+    const [mount, setMount] = useState(true);
 
-        </section>
+    return(
+
+        <AnimatePresence>
+            {mount && 
+            <motion.section 
+                className={styles.loading}
+                exit={{scaleY: 0}}
+                transition={{duration: 0.7}}
+                >
+                <div className={styles.content}>
+                    <h2>Hold on.. let me think.</h2>
+                    <ProgressBar setMount={setMount}/>              
+                </div>
+            </motion.section>
+            }           
+        </AnimatePresence>
+
     )
 }
 
--- src/Components/LoadingScreen/ProgressBar/ProgressBar.js
+++ src/Components/LoadingScreen/ProgressBar/ProgressBar.js
@@ -1,33 +1,83 @@
-import React, {useEffect, useState, useRef} from 'react';
+import React, {useEffect, useState} from 'react';
+import projectImages from '~/Components/ProjectSection/ProjectData/images'
+import OuterMostRing from '~/Components/AnimateBackgroundPattern/AnimateSequence/OuterMostRing/images';
+import OuterRing from '~/Components/AnimateBackgroundPattern/AnimateSequence/OuterRing/images';
+import InnerRing from '~/Components/AnimateBackgroundPattern/AnimateSequence/InnerRing/images';
+import CenterRing from '~/Components/AnimateBackgroundPattern/AnimateSequence/CenterRing/images';
+import InnerMostRing from '~/Components/AnimateBackgroundPattern/AnimateSequence/InnerRing/images';
+import OuterMostSquare from '~/Components/AnimateBackgroundPattern/AnimateSequence/AnimateSquarePattern/OuterMostSquare/images';
+import OuterSquare from '~/Components/AnimateBackgroundPattern/AnimateSequence/AnimateSquarePattern/OuterSquare/images';
+import InnerSquare from '~/Components/AnimateBackgroundPattern/AnimateSequence/AnimateSquarePattern/InnerSquare/images';
+import InnerMostTriangle from '~/Components/AnimateBackgroundPattern/AnimateSequence/AnimateSquarePattern/InnerSquare/images';
+import contactMeIcons from '~/Components/ContactMeSection/icons';
+import assets from '~/assets/icons';
 import * as styles from './styles.module.css';
 
-function ProgressBar(){
+function ProgressBar({setMount}){
     const [percent, setPercent] = useState(0);
-    const interval = useRef();
+
+    const preloadImages = () => {
+        const allImages = [
+            ...Object.entries(projectImages), 
+            ...Object.entries(OuterMostRing), 
+            ...Object.entries(OuterRing),
+            ...Object.entries(InnerRing),
+            ...Object.entries(CenterRing),
+            ...Object.entries(InnerMostRing),
+            ...Object.entries(OuterMostSquare),
+            ...Object.entries(OuterSquare),
+            ...Object.entries(InnerSquare),
+            ...Object.entries(InnerMostTriangle),
+            ...Object.entries(assets),
+            ...Object.entries(contactMeIcons),
+        ];
+        const n = allImages.length;
+
+        const promises = allImages.map(([name, src], i) => {
+            return new Promise((resolve, reject) => {
+                const image = new Image();
+                image.src = src;
+                image.onload = () => {
+                    resolve();
+                    setTimeout(() => {
+                        setPercent((prev) => Math.ceil(prev + ((1/n) * 100)));                        
+                    }, i * 100)
+                }
+                image.onerror = () => {
+                    reject(new Error(`${name} image couldn't be loaded`));
+                }
+            })
+        })
+
+        return Promise.all(promises);
+    }
 
     useEffect(() => {
-        interval.current = setInterval(() => {
-            setPercent((prevPercent) => {
-                if(prevPercent < 99)
-                    return prevPercent + 1;
-                else
-                    return 100;
+        preloadImages()
+            .then(() => {
+                console.log('All images have been pre-loaded');
+                //setPercent(100);
             })
-        }, 100)
+            .catch((error) => {
+                const message = error.message;
+                console.error('Error occurred while pre-loading images', message);
+            })
     }, [])
+
 
     useEffect(() => {
         if(percent === 100)
-            clearInterval(interval.current);
+            setMount(false);
+    
     }, [percent])
 
     return(
         <>
             <div className={styles.progress}>
-                <div className={styles.progress_bar} style={{width: `${percent}%`}}></div>
+                <div className={styles.progress_bar} style={{width: `${Math.ceil(percent)}%`}}></div>
             </div>        
             <h3 className={styles.progress_percent}>
-                {`[${percent}]%`}
+                {`Loading [${percent}]%`}
             </h3>  
         </>
 
