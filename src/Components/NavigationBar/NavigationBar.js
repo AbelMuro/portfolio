772b853682601c5a90734c04a52e84d422b227f9
abelmuro <abelmuro93@gmail.com>
2025-10-16 10:15:48 -0700

    commit

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -37,10 +37,10 @@ function NavigationBar() {
             window.scrollTo({top: 5500, behavior: 'smooth'});
         else{
             window.scrollTo({top: 32289, behavior: 'instant'});
-            window.scrollTo({top: 32289, behavior: 'instant'});
+            setTimeout(() => {
+                window.scrollTo({top: 32289, behavior: 'instant'});
+            }, 10)
         }
-            
-     
     }
 
     useMotionValueEvent(scrollYProgress, 'change', () => {
