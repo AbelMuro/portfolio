701d9db8a630ebe9098fcebf5e07985405ea9666
abelmuro <abelmuro93@gmail.com>
2025-10-15 23:20:13 -0700

    commit

--- src/Components/Mobile/MobileBackgroundPattern/MobileRingPattern/images/index.js
+++ src/Components/Mobile/MobileBackgroundPattern/MobileRingPattern/images/index.js
@@ -1,6 +1,6 @@
 import outerRing from './outer ring.png';
 import outerMostRing from './outer most ring.png';
-import centerRing from './centerRing.png';
+import centerRing from './CenterRing.png';
 import innerRing from './inner ring.png';
 import innerMostRing from './inner most ring.png';
 
